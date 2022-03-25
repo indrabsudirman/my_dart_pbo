90068fb3d901f2a2ef1bb12cab4a350be5676737
indrabsudirman <indrabsudirman@gmail.com>
2022-03-25 16:11:37 +0700

    First Commit

---
+++ bin/my_dart_pbo.dart
@@ -0,0 +1,5 @@
+import 'package:my_dart_pbo/my_dart_pbo.dart' as my_dart_pbo;
+
+void main(List<String> arguments) {
+  print('Hello world: ${my_dart_pbo.calculate()}!');
+}
---
+++ test/my_dart_pbo_test.dart
@@ -0,0 +1,8 @@
+import 'package:my_dart_pbo/my_dart_pbo.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
