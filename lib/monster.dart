d3afa0173579c5541c43dfe54f87bf8e6f733c0e
Indra Sudirman <indrabsudirman@gmail.com>
2022-03-26 09:43:27 +0700

    Inheritance

---
+++ lib/monster.dart
@@ -0,0 +1,13 @@
+class Monster {
+  int? _healthPoints;
+
+  int get healthPoints => _healthPoints!;
+  set healthPoints(int value) {
+    if (value <= 0) {
+      throw ArgumentError('Health Points harus positif dan tidak boleh kosong');
+    }
+    _healthPoints = value;
+  }
+
+  String eatHuman() => 'Hmmmmm .. I\'m eating human';
+}
